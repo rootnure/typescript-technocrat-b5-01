71b4708b0a780462bd867a9fa5ce6abc94f4a72d
rootnure <nur.diu.2791@gmail.com>
2025-05-01 07:59:19 +0600

    Non-Primitive types (Array and Tuple) in TS

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 ## Basic Data Types in TS
 - Primitive types: string, number, boolean, undefined, null
+```ts
+let firstName : string = 'Nure Alom'; // string
+let age : number = 26; // number
+let isAdmin = true; // boolean
+let x : undefined = undefined; // undefined
+let z : null = null; // null
+```
 - Non-primitive types: array, tuple, object
+```ts
+let friends : string[] = ['Ashiq', 'Bablu', 'Hablu']; // string type array
+let eligibleIds : number[] = [2411, 6457, 8790]; // number type array
+let coordinates : [number, number] = [23.27, 39.19]; // Tuple with same typs of data
+let user : [string, number, boolean] = ['Nure Alom', 26, true]; // Tuple with different types of data
+```
--- module1/src/basic-data-types.ts
+++ module1/src/basic-data-types.ts
@@ -1,5 +1,6 @@
 /* Basic Data Type */
 
+/* Primitive Types */
 // string
 let firstName : string = 'Nure Alom';
 
@@ -14,3 +15,23 @@ let x : undefined = undefined;
 
 // null
 let z : null = null;
+
+let d: string; // only string type data can be stored in this variable now
+d = 'abc'; // ✅
+// d = 123 // ❌
+// d = false; // ❌
+
+/* Non-primitive Types */
+// Array
+let friends : string[] = ['Ashiq', 'Bablu', 'Hablu']; // string type array
+friends.push('Dablu'); // ✅
+// friends.push(22); // ❌
+
+let eligibleIds : number[] = [2411, 6457, 8790]; // number type array
+eligibleIds.push(9122); // ✅
+// eligibleIds.push('WEB8-2555'); // ❌
+
+// Tuple --> array --> order --> type of value
+// Special type of array with fixed length and every index has meaning
+let coordinates : [number, number] = [23.21447, 39.91419]; // [0] --> latitude, [1] --> longitude
+let user : [string, number, boolean] = ['Nure Alom', 26, true]; // [0] --> name, [1] --> age, [2] --> isAdmin
