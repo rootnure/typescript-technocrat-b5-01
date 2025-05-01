bb724a792750a0b740f1821bc370955da17d7a98
rootnure <nur.diu.2791@gmail.com>
2025-05-01 07:31:03 +0600

    Primitive types in TS

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typescript Technocrad 01
+
+## Basic Data Types in TS
+- Primitive types: string, number, boolean, undefined, null
+- Non-primitive types: array, tuple, object
---
+++ module1/src/basic-data-types.ts
@@ -0,0 +1,16 @@
+/* Basic Data Type */
+
+// string
+let firstName : string = 'Nure Alom';
+
+// number
+let age : number = 26;
+
+// boolean
+let isAdmin = true;
+
+// undefined
+let x : undefined = undefined;
+
+// null
+let z : null = null;
